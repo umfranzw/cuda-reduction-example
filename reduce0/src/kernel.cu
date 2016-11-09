bd330c41a9591f952cd428726b01d2efe15d0294
Wayne <umfranzw@cs.umanitoba.ca>
2016-11-09 13:54:51 -0600

    Fixed grammar and spelling errors in comments. Renamed some kernel variables.

--- reduce0/src/kernel.cu
+++ reduce0/src/kernel.cu
@@ -9,7 +9,7 @@
  *
  * Notes on kernel args: the arguments passed in for the arrays must be *device buffers* (not host buffers)!
  * n is an integer that is passed in from the host when the kernel is launched.
- * No cudaMemCpy is require to do this (cudaMemCpy is only needed for non-primitive data types).
+ * No cudaMemCpy is require to do this (args on the stack are copied).
  */
 __global__ void reduce(float *input, float *output, unsigned int n)
 {
@@ -24,18 +24,18 @@ __global__ void reduce(float *input, float *output, unsigned int n)
     
     // Perform the reduction using "interleaved indexing" (each thread adds a pair of
     // elements right next to each other).
-    // The stride is the distance between the elements that each thread adds.
+    // "stride" is the distance between the elements that each thread adds.
     // This distance doubles on each iteration.
-    // Note that the number of threads required also halves on each iteration.
+    // The number of threads required halves on each iteration.
     unsigned int left;  // holds index of left operand
     unsigned int right; // holds index or right operand
     for (unsigned int stride = 1; stride < block_size * 2; stride *= 2)
     {
         left = index + local_id * stride * 2;
         right = left + stride;
-        // we may be running more threads than we need
+
         if (right - index < block_size * 2 // read: "If this thread should be
-                                           // active on this iteration."
+                                           // active on this iteration of the reduction."
             && right < n) // If we're the last block, we may be running more threads
                           // than we need - this condition makes sure they dont interfere.
         {
@@ -48,8 +48,8 @@ __global__ void reduce(float *input, float *output, unsigned int n)
     }
 
     // Once the loop is done, the partial sum for this block will be in the leftmost index
-    // for this block's chunk. The code below causes each block's thread 0 to write that
-    // partial result to the output buffer at the index given by its block_id. After the code
+    // of this block's chunk. The code below causes each block's thread 0 to write that
+    // partial result to the output buffer at position "block_id". After the code
     // below completes, the output buffer will contain exactly <number of blocks>
     // consecutive partial results.
     if (!local_id)
